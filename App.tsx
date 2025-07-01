96e5b0efcd8891ce5e4c92a21394c19365ef5af9
Felipe Frizzo <felipefrizzovg@gmail.com>
2025-07-01 12:11:14 -0300

    gluestack-ui setup

--- App.tsx
+++ App.tsx
@@ -1,4 +1,6 @@
 import { Text, View, StatusBar } from "react-native";
+import "@/global.css";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import {
   useFonts,
   Roboto_700Bold,
@@ -11,21 +13,22 @@ export default function App() {
     Roboto_400Regular,
   });
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: '#202024'
-      }}
-    >
-      <StatusBar 
-        barStyle={"light-content"}
-        backgroundColor='transparent'
-        translucent
-      />
-
-      { fontsLoaded ? <Text>Home</Text> : <View /> }
-    </View>
+    <GluestackUIProvider mode="light">
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: '#202024'
+        }}
+      >
+        <StatusBar 
+          barStyle={"light-content"}
+          backgroundColor='transparent'
+          translucent
+        />
+        { fontsLoaded ? <Text>Home</Text> : <View /> }
+      </View>
+    </GluestackUIProvider>
   );
 }
